7042571e655aed2f7d2e3a735e273a9ece013ce1
vadimula <vadimula@inbox.ru>
2023-11-04 21:35:12 +0900

    3-th part

--- file1.py
+++ file1.py
@@ -9,7 +9,7 @@ for line in data:
         rec_data = str(datetime.strptime(line.strip(), '%d.%m.%Y; %H:%M'))
         res.setdefault(rec_data, [])
     elif line.strip():
-        res.setdefault(rec_data, []).append(line.strip())
+        res.setdefault(rec_data).append(line.strip())
 
 with open('file1.json', 'w', encoding='utf-8') as file:
     json.dump(res, file, indent=3, ensure_ascii=False)
